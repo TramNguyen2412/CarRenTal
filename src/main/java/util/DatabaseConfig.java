e919b36afec4731f3f5313b9f6c231b0229f8836
TramNguyen <ngoctram0380@gmail.com>
2025-06-09 17:55:06 +0700

    Giống main

--- src/main/java/util/DatabaseConfig.java
+++ src/main/java/util/DatabaseConfig.java
@@ -1,10 +1,9 @@
 package util;
 
 public class DatabaseConfig {
-
-    public static final String URL = "jdbc:oracle:thin:@//localhost:1521/FREEPDB1";
-    public static final String USERNAME = "IS216";
-    public static final String PASSWORD = "abc";
+  public static final String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+    public static final String USERNAME = "TESTDOANSO1";
+    public static final String PASSWORD = "abc123";
 
     public static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
 }
